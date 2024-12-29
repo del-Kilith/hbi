5b9f9ac9e0e14e909b6526960bf74669b533cd70
del-Kilith <64635315+del-Kilith@users.noreply.github.com>
2024-12-29 23:07:42 +0100

    Add files via upload

--- debug.js
+++ debug.js
@@ -1,5 +1,8 @@
 const isDebugMode = location.hostname.match(/localhost/)
 
+console.log('Debug mode:', isDebugMode)
+console.log('Version: 0.0.1')
+
 if (!isDebugMode) {
   console.log = () => {}
   console.info = () => {}
